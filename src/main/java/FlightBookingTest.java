e8c3067a4a52d004e190c19c9e0c0f1630fcb30d
MinuBharathy <38165347+MinuBharathy@users.noreply.github.com>
2018-04-08 19:59:54 +0530

    Add files via upload

--- src/main/java/FlightBookingTest.java
+++ src/main/java/FlightBookingTest.java
@@ -18,9 +18,9 @@ public class FlightBookingTest {
     @Test
     public void testThatResultsAppearForAOneWayJourney() {
 
-        setDriverPath();
+        Utils.setDriverPath();
         driver.get("https://www.cleartrip.com/");
-        waitFor(2000);
+        Utils.waitForId(driver, "OneWay", 10);
         driver.findElement(By.id("OneWay")).click();
 
         driver.findElement(By.id("FromTag")).clear();
@@ -28,16 +28,16 @@ public class FlightBookingTest {
 
         //wait for the auto complete options to appear for the origin
 
-        waitFor(2000);
+        Utils.waitForId(driver, "ui-id-1", 10);
         List<WebElement> originOptions = driver.findElement(By.id("ui-id-1")).findElements(By.tagName("li"));
         originOptions.get(0).click();
 
-        driver.findElement(By.id("toTag")).clear();
-        driver.findElement(By.id("toTag")).sendKeys("Delhi");
+        driver.findElement(By.id("ToTag")).clear();
+        driver.findElement(By.id("ToTag")).sendKeys("Delhi");
 
         //wait for the auto complete options to appear for the destination
 
-        waitFor(2000);
+        Utils.waitForId(driver, "ui-id-2", 10);
         //select the first item from the destination auto complete list
         List<WebElement> destinationOptions = driver.findElement(By.id("ui-id-2")).findElements(By.tagName("li"));
         destinationOptions.get(0).click();
@@ -47,24 +47,13 @@ public class FlightBookingTest {
         //all fields filled in. Now click on search
         driver.findElement(By.id("SearchBtn")).click();
 
-        waitFor(5000);
+        Utils.waitForClassName(driver, "searchSummary", 10);
         //verify that result appears for the provided journey search
         Assert.assertTrue(isElementPresent(By.className("searchSummary")));
 
         //close the browser
         driver.quit();
-
     }
-
-
-    private void waitFor(int durationInMilliSeconds) {
-        try {
-            Thread.sleep(durationInMilliSeconds);
-        } catch (InterruptedException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-    }
-
 
     private boolean isElementPresent(By by) {
         try {
@@ -75,15 +64,4 @@ public class FlightBookingTest {
         }
     }
 
-    private void setDriverPath() {
-        if (PlatformUtil.isMac()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
-        }
-        if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        }
-        if (PlatformUtil.isLinux()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
-        }
-    }
 }
