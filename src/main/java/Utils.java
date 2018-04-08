2172801633f02991385ae5e7c1c2b5b778280d18
MinuBharathy <38165347+MinuBharathy@users.noreply.github.com>
2018-04-08 20:03:39 +0530

    Add files via upload

---
+++ src/main/java/Utils.java
@@ -0,0 +1,34 @@
+import com.sun.javafx.PlatformUtil;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Utils {
+
+    protected static void setDriverPath() {
+        if (PlatformUtil.isMac()) {
+            System.setProperty("webdriver.chrome.driver", "chromedriver");
+        }
+        if (PlatformUtil.isWindows()) {
+            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        }
+        if (PlatformUtil.isLinux()) {
+            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
+        }
+    }
+    protected static void waitForId(WebDriver driver, String locatorId, int durationInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, durationInSeconds);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(locatorId)));
+    }
+
+    protected static void waitForLinkText(WebDriver driver, String locatorId, int durationInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, durationInSeconds);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText(locatorId)));
+    }
+
+    protected static void waitForClassName(WebDriver driver, String locatorId, int durationInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, durationInSeconds);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className(locatorId)));
+    }
+}
